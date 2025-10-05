500e24e658fc1cf723d9fec5fa26715257283e62
Théotime DUSERRE <98874870+Balansoire@users.noreply.github.com>
2025-10-05 17:05:44 +0200

    Ajout de la fonctionnalité de filtrage des vulnérabilités et mise à jour des compteurs de vulnérabilités dans le tableau de bord

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NgFor, NgClass } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NgFor, NgClass],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -25,7 +25,26 @@ export class DashboardComponent implements OnInit {
   loadVulnerabilities() {
     this.httpClient.get(this.vulnerabilityUrl).subscribe((data: any) => {
       this.vulnerabilities = data;
+      this.countVulnerabilities();
     });
+  }
+
+  countVulnerabilities() {
+    this.vulnerabilityCounts = {
+      all: this?.vulnerabilities.length || 0,
+      critical: this?.vulnerabilities.filter(v => v.severity === 'CRITICAL').length || 0,
+      high: this?.vulnerabilities.filter(v => v.severity === 'HIGH').length || 0,
+      medium: this?.vulnerabilities.filter(v => v.severity === 'MEDIUM').length || 0,
+      low: this?.vulnerabilities.filter(v => v.severity === 'LOW').length || 0,
+    };
+  }
+
+  shownVulnerabilities() {
+    if (this.filter === 'ALL') {
+      return this.vulnerabilities;
+    } else {
+      return this.vulnerabilities.filter(v => v.severity === this.filter);
+    }
   }
 
   goToIncidentForm() {
@@ -34,13 +53,15 @@ export class DashboardComponent implements OnInit {
 
   vulnerabilityUrl = 'http://localhost:5000/api/vulnerabilities';
 
+  filter: 'ALL' | 'CRITICAL' | 'HIGH' | 'MEDIUM' | 'LOW' = 'ALL';
+
   vulnerabilities: any[] = [];
 
   vulnerabilityCounts = {
-    all: this?.vulnerabilities.length || 0,
-    critical: this?.vulnerabilities.filter(v => v.severity === 'Critical').length || 0,
-    high: this?.vulnerabilities.filter(v => v.severity === 'High').length || 0,
-    medium: this?.vulnerabilities.filter(v => v.severity === 'Medium').length || 0,
-    low: this?.vulnerabilities.filter(v => v.severity === 'Low').length || 0,
+      all: 0,
+      critical: 0,
+      high: 0,
+      medium: 0,
+      low: 0,
   };
 }
